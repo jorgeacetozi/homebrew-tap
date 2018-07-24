4eeae4e01ab1fd89e647ed0011918b4a134f9113
goreleaserbot <goreleaser@carlosbecker.com>
2018-07-24 18:27:29 -0300

    Brew formula update for test-cli version 0.16.0

--- Formula/test-cli.rb
+++ Formula/test-cli.rb
@@ -1,9 +1,9 @@
 class TestCli < Formula
   desc "Simple test CLI in Go."
   homepage "https://jorgeacetozi.com/"
-  url "https://github.com/jorgeacetozi/test-cli/releases/download/0.15.0/test-cli_0.15.0_Darwin_x86_64.tar.gz"
-  version "0.15.0"
-  sha256 "898053d552ac85258c96c4a95e7e743bf4ed30c98db2525020a0304bd1aa854e"
+  url "https://github.com/jorgeacetozi/test-cli/releases/download/0.16.0/test-cli_0.16.0_Darwin_x86_64.tar.gz"
+  version "0.16.0"
+  sha256 "c287f8e571743f3013157deb14b92c7bae254f09e1a70c8713bc321f34cebdc5"
 
   def install
     bin.install "test-cli"
